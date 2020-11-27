ea314e53bf0c2d9f34670afc14e1275fe5c55596
Kingsley <kingsleyumujeyan@gmail.com>
2020-11-27 08:25:35 +0100

    ft--backend created

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGOdb } = require("./config");
+
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+mongoose.connect(`${MONGOdb}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Mongodb connected!")
+        return server.listen({ port: 5000 })
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`)
+    });
+
+
+    
