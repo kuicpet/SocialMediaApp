7a3c3d24d771874abee2f2e92598d141f082b203
Kingsley <kingsleyumujeyan@gmail.com>
2020-11-27 19:21:49 +0100

    ft--delete Post logic added

--- backend/graphql/resolvers/posts.js
+++ backend/graphql/resolvers/posts.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server");
+
 const Post = require("../../models/Post");
 const checkAuth = require("../../utils/check-auth");
 
@@ -37,6 +39,20 @@ module.exports = {
          });
          const post = await newPost.save();
          return post;
+        },
+        async deletePost(_, { postId }, context){
+            const user = checkAuth(context);
+            try {
+                const post = await Post.findById(postId);
+                if(user.username === post.username){
+                    await post.delete();
+                    return "Post deleted successfully!";
+                } else {
+                    throw new AuthenticationError("Action not allowed")
+                }
+            } catch (error) {
+                throw new Error(error);
+            }
         }
     }
 };
