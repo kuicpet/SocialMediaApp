a675937d101d69bdeaf3d6b2e8ef9230806c0ddf
Kingsley <kingsleyumujeyan@gmail.com>
2020-11-27 22:12:22 +0100

    bg--updated post body validation

--- backend/graphql/resolvers/posts.js
+++ backend/graphql/resolvers/posts.js
@@ -29,7 +29,10 @@ module.exports = {
     Mutation: {
         async createPost(_, { body }, context){
            const user = checkAuth(context);
-           console.log(user);
+           //console.log(user);
+           if(arguments.body.trim() === ""){
+              throw new Error("Post body must not be empty");
+           }
 
            const newPost = new Post({
                body,
