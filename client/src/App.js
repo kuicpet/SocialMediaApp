a3ded8240fb7ae5cb0bef1e123c4392c24eaca68
Kingsley <kingsleyumujeyan@gmail.com>
2020-11-28 00:49:13 +0100

    ft--navbar,routing added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Container } from 'semantic-ui-react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
 import './App.css';
+
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
 
 function App() {
   return (
-   <div>
-     <h1>Social Media App</h1>
-   </div>
+  
+   <Router>
+     <Container>
+        <Navbar/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+     </Container>
+   </Router>
   );
 }
 
