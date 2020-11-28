7176ee47b472e8fec9e00b8fe4387a1e5b5062fe
Kingsley <kingsleyumujeyan@gmail.com>
2020-11-28 09:20:27 +0100

    ft--added likes and comments buttons to post

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Card, Icon, Label, Image } from "semantic-ui-react";
+import { Card, Icon, Label, Button, Image } from "semantic-ui-react";
 import moment from "moment";
 import { Link } from "react-router-dom";
 
 function PostCard({ post: { id, body, createdAt, username, likeCount, commentCount, likes } }) {
+
+    function likePost(){
+        console.log("Like Post");
+    }
+
+    function commentOnPost(){
+        console.log("Comment on Post");
+    }
+
     return (
         <Card  fluid>
         <Card.Content>
@@ -17,7 +26,22 @@ function PostCard({ post: { id, body, createdAt, username, likeCount, commentCou
           <Card.Description>{body}</Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <p>buttons here</p>
+        <Button as='div' labelPosition='right' onClick={likePost}>
+          <Button color='red' basic>
+              <Icon name='heart' />
+            </Button>
+            <Label basic color='red' pointing='left'>
+              {likeCount}
+            </Label>
+          </Button>
+          <Button as='div' labelPosition='right' onClick={commentOnPost}>
+            <Button basic color='blue'>
+              <Icon name='comments' />
+            </Button>
+            <Label as='a' basic color='blue' pointing='left'>
+              {commentCount}
+            </Label>
+          </Button>
         </Card.Content>
       </Card>
     )
