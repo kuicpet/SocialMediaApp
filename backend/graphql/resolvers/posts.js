4e65e7f138bec63d0740cd65b06173bd7c332cb1
Kingsley <kingsleyumujeyan@gmail.com>
2020-12-01 08:15:48 +0100

    ft--updated backend

--- backend/graphql/resolvers/posts.js
+++ backend/graphql/resolvers/posts.js
@@ -30,7 +30,7 @@ module.exports = {
         async createPost(_, { body }, context){
            const user = checkAuth(context);
            //console.log(user);
-           if(arguments.body.trim() === ""){
+           if(body.trim() === ""){
               throw new Error("Post body must not be empty");
            }
 
