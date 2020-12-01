dcdf11f15ff7dd58f3c0a6799045f5b9b1930374
Kingsley <kingsleyumujeyan@gmail.com>
2020-12-01 07:10:33 +0100

    ft--updated validation error for post

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -29,7 +29,8 @@ function PostForm() {
     };
 
     return (
-        <Form onSubmit={onSubmit}>
+        <>
+            <Form onSubmit={onSubmit}>
             <h2>Create a Post</h2>
             <Form.Field>
                 <Form.Input 
@@ -37,12 +38,23 @@ function PostForm() {
                     name="body"
                     onChange={onChange}
                     value={values.body}
+                    error={error ? true : false}
                 />
                 <Button type="submit" color="blue">
                     Submit
                 </Button>
             </Form.Field>
         </Form>
+        { error && (
+            <div className="ui error message" style={{ marginBottom: 20 }}>
+               <ul className="list">
+                    <li>
+                        {error.graphQLErrors[0].message}
+                    </li>
+                </ul> 
+            </div>
+        )}
+        </>
     );
 }
 
