f1abe887d7dcf3b7e8cf491174da9d1d8b644d20
Kingsley <kingsleyumujeyan@gmail.com>
2020-12-02 17:05:05 +0100

    ft--updated

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -134,7 +134,7 @@ function Post(props) {
 }
 
 const SUBMIT_COMMENT_MUTATION = gql`
-    mutation($postId: String!, $body: String!){
+    mutation($postId: ID!, $body: String!){
         createComment(postId: $postId, body: $body){
             id
             comments{
