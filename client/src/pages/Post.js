ba21619d7c7146ccbed5a8da9ac1aea31104be8c
Kingsley <kingsleyumujeyan@gmail.com>
2020-12-02 08:28:13 +0100

    ft--updated delete button logic

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -66,6 +66,18 @@ function Post(props) {
                             )}
                         </Card.Content>
                       </Card>
+                      {comments.map(comment => (
+                          <Card fluid key={comment.id}>
+                              <Card.Content>
+                                  {user && user.username === comment.username && (
+                                      <DeleteButton postId={id} commentId={comment.id} />
+                                  )}
+                                  <Card.Header>{comment.username}</Card.Header>
+                                  <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                                  <Card.Description>{comment.body}</Card.Description>
+                              </Card.Content>
+                          </Card>
+                      ))}
                    </Grid.Column>
                </Grid.Row>
            </Grid>
